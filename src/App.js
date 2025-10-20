09302d2750c8ab38c58dd9e9613c6659589fd3b2
adirathodd <88114611+adirathodd@users.noreply.github.com>
2025-10-19 22:27:38 -0400

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,91 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import * as d3 from "d3"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {wordFrequency:[]};
+  }
+  componentDidMount() {
+    this.renderChart();
+  }
+
+  componentDidUpdate() {
+    this.renderChart();
+  }
+
+  getWordFrequency = (text) => {
+    const stopWords = new Set(["the", "and", "a", "an", "in", "on", "at", "for", "with", "about", "as", "by", "to", "of", "from", "that", "which", "who", "whom", "this", "these", "those", "it", "its", "they", "their", "them", "we", "our", "ours", "you", "your", "yours", "he", "him", "his", "she", "her", "hers", "it", "its", "we", "us", "our", "ours", "they", "them", "theirs", "I", "me", "my", "myself", "you", "your", "yourself", "yourselves", "was", "were", "is", "am", "are", "be", "been", "being", "have", "has", "had", "having", "do", "does", "did", "doing", "a", "an", "the", "as", "if", "each", "how", "which", "who", "whom", "what", "this", "these", "those", "that", "with", "without", "through", "over", "under", "above", "below", "between", "among", "during", "before", "after", "until", "while", "of", "for", "on", "off", "out", "in", "into", "by", "about", "against", "with", "amongst", "throughout", "despite", "towards", "upon", "isn't", "aren't", "wasn't", "weren't", "haven't", "hasn't", "hadn't", "doesn't", "didn't", "don't", "doesn't", "didn't", "won't", "wouldn't", "can't", "couldn't", "shouldn't", "mustn't", "needn't", "daren't", "hasn't", "haven't", "hadn't"]);
+    const words = text.toLowerCase().replace(/[.,/#!$%^&*;:{}=_`~()]/g, "").replace(/\s{2,}/g, " ").split(" ");
+    const filteredWords = words.filter(word => !stopWords.has(word));
+    return Object.entries(filteredWords.reduce((freq, word) => {
+      freq[word] = (freq[word] || 0) + 1;
+      return freq;
+    }, {}));
+  }
+
+  renderChart() {
+    const data = this.state.wordFrequency
+      .sort((a, b) => b[1] - a[1])
+      .slice(0, 5);
+
+    const width = 1000, height = 250;
+    const margin = width * 0.15;
+    const svg = d3.select('.svg_parent').attr('width', width).attr('height', height);
+    const xScale = d3.scaleLinear().domain([0, data.length - 1]).range([margin, 1000 - margin]);
+    const fontSizeScale = d3.scaleLinear().domain(d3.extent(data, d => d[1])).range([20, 70]);
+
+    svg.selectAll('text')
+      .data(data, d => d[0])
+      .join(
+        // enter phase to create new elements
+        enter => enter.append('text')
+          .text(d => d[0])
+          .attr('x', (d, i) => xScale(i))
+          .attr('y', 125)
+          .attr('text-anchor', 'middle')
+          .style('fill', 'steelblue')
+          .style('opacity', 0)
+          .style('font-size', 0)
+          .transition()
+            .duration(1000)
+            .style('opacity', 1)
+            .style('font-size', d => `${fontSizeScale(d[1])}px`),
+
+        // update phase to resize and reposition words
+        update => update
+          .transition()
+          .duration(1000)
+          .attr('x', (d, i) => xScale(i))
+          .attr('y', 125)
+          .attr('text-anchor', 'middle')
+          .style('font-size', d => `${fontSizeScale(d[1])}px`),
+
+        // exit phase to fade out and remove older
+        exit => exit
+          .transition()
+          .duration(500)
+          .style('opacity', 0)
+          .remove()
+      );
+  }
+
+  render() {
+    return (
+      <div className="parent">
+        <div className="child1" style={{width: 1000 }}>
+        <textarea type="text" id="input_field" style={{ height: 150, width: 1000 }}/>
+          <button type="submit" value="Generate Matrix" style={{ marginTop: 10, height: 40, width: 1000 }} onClick={() => {
+                var input_data = document.getElementById("input_field").value
+                this.setState({wordFrequency:this.getWordFrequency(input_data)})
+              }}
+            > Generate WordCloud</button>
+        </div>
+        <div className="child2"><svg className="svg_parent"></svg></div>
+      </div>
+    );
+  }
 }
 
 export default App;
